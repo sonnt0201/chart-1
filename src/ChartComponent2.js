420efbbb1768bd5724adfec7e5924d64267667fc
Duc22102003 <152000441+Duc22102003@users.noreply.github.com>
2024-01-03 22:38:56 +0700

    Update ChartComponent2.js

--- src/ChartComponent2.js
+++ src/ChartComponent2.js
@@ -1,27 +1,41 @@
 import React, { useEffect, useState } from "react";
 import { Line } from "react-chartjs-2";
+import Chart from "chart.js/auto";
 import axios from "axios";
 
 function App() {
   const [data, setData] = useState(null);
   const [chartData, setChartData] = useState({ labels: [], datasets: [] });
-
+ const [timestamp, setTimestamp]=useState(Math.floor(new Date().getTime()/1000));
   const [isRunning, setIsRunning] = useState(true);
-  const [timestamp, setTimestamp] = useState(new Date().getTime()/1000);
 
+
+
+
+  // call whenever begin changes
   useEffect(() => {
     fetchData();
+    // call fetch function
   }, [timestamp]);
 
   useEffect(() => {
+    // setInterval goes here.
+    // Interval does increase "begin" ONLY
     const intervalId = setInterval(() => {
       if (isRunning) {
-        setTimestamp(new Date().getTime()/1000);
+        setTimestamp(Math.floor(new Date().getTime()/1000));
       }
     }, 1000);
 
     return () => clearInterval(intervalId);
   }, [isRunning]);
+
+  // fixed
+  // update chartData whenever data changes
+  useEffect(() => {
+    console.log(data);
+    update();
+  }, [data]);
 
   const fetchData = async () => {
     const response = await axios.get(
@@ -29,16 +43,53 @@ function App() {
     );
 
     responseToDataObject(response.data);
+    // setRequestCount((prevCount) => prevCount + 1);
   };
 
   const responseToDataObject = (responseData) => {
-    // Process the data and update the state
+    const payload = responseData.payload;
+
+    const arr = [];
+    payload.forEach((record) => {
+      var start = record.timestamp * 1000;
+      const vols = record.voltages;
+      vols.forEach((vol) => {
+        arr.push({
+          time: start,
+          voltage: vol,
+        });
+        start += 10;
+      });
+    });
+
+    console.log("array: ", arr);
+    setData(arr);
+  };
+
+  const update = () => {
+    if (data)
+      setChartData({
+        labels: data.map((entry) => entry.time),
+        datasets: [
+          {
+            label: "Voltage",
+            data: data.map((entry) => entry.voltage),
+            fill: false,
+            borderColor: "rgba(75,192,192,1)",
+            tension: 0.1,
+          },
+        ],
+      });
+  };
+
+  const toggleRunning = () => {
+    setIsRunning((prevIsRunning) => !prevIsRunning);
   };
 
   return (
     <div>
-      <h1>Đồ thị 2</h1>
-      <button onClick={() => setIsRunning(!isRunning)}>
+      <h1>Đồ thị 1</h1>
+      <button onClick={toggleRunning}>
         {isRunning ? "Dừng" : "Tiếp tục"}
       </button>
       {chartData && <Line data={chartData} />}
